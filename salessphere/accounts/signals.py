b58510d0e817f1fd69ef5b8901f4f77aed58eac5
waqarshaikh <shaikhwaqar1999@gmail.com>
2024-03-08 18:07:00 -0500

    create signals.py

---
+++ salessphere/accounts/signals.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+from .models import Employee
+
+
+def employee_profile(sender, instance, created, **kwargs):
+    if created:
+        Employee.objects.create(user=instance, name=instance.username, email=instance.email)
+    print('Profile created!')
+
+
+post_save.connect(employee_profile, sender=User)
