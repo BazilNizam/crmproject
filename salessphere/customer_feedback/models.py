e6ebf7ebc3c1ca6817f133e5ecf1d4c4009cb417
Sumit Sohal <sumitsohal2018@gmail.com>
2024-04-01 16:05:00 -0400

    add models

--- salessphere/customer_feedback/models.py
+++ salessphere/customer_feedback/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from accounts.models import Product, Employee
 
-# Create your models here.
+
+class CustomerFeedback(models.Model):
+    stars = (
+        ('1 Star', '1 Star'),
+        ('2 Stars', '2 Stars'),
+        ('3 Stars', '3 Stars'),
+        ('4 Stars', '4 Stars'),
+        ('5 Stars', '5 Stars'),
+    )
+    name = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=20, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    product = models.ManyToManyField(Product)
+    feedback = models.TextField(max_length=500, null=True)
+    rate = models.CharField(max_length=10, choices=stars, default='5 Stars')
+
+    def __str__(self):
+        return str(self.name)
+
+class IntrestedCustomer(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=20, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    product = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.name
+
+class CustomerComplaint(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=20, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    product = models.ManyToManyField(Product)
+    employee = models.ManyToManyField(Employee)
+    message = models.TextField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+$Env:GIT_AUTHOR_DATE="2024-03-31T16:05:00"
+$Env:GIT_COMMITTER_DATE="2024-03-31T16:05:00"
+git commit -m "add models"
